45c70e48cac2b941282bf306fa3f2e2fec67b85f
elahe <elaheyaghoobi1@gmail.com>
2022-08-23 12:03:00 +0430

    creating files

---
+++ Configs.hpp
@@ -0,0 +1,18 @@
+#ifndef CONFIGS_HPP
+#define CONFIGS_HPP
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace std;
+using namespace cv;
+
+extern Mat sourceImage;
+extern Mat grayscaled;
+extern Mat gFiltered;
+extern Mat sFiltered;
+extern Mat non;
+extern Mat angles;
+extern Mat finalResult; 
+
+#endif // CONFIGS_HPP
---
+++ canny.hpp
@@ -0,0 +1,15 @@
+#ifndef CANNY_HPP
+#define CANNY_HPP
+
+#include "Configs.hpp"
+
+class Canny{
+    public:
+        Canny(string );
+        Mat gaussianBlur(Mat );
+    
+        
+}
+
+#endif //CANNY.HPP
+
